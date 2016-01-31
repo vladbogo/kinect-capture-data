e0e1446d64b40c17fbfa356d69e8d1beaa03deb8
Vlad Bogolin <vladbogolin@gmail.com>
2016-01-31 20:35:37 +0200

    Draw skeleton correctly
    Draw skeleton correctly

--- bin/Debug/drawskt.m
+++ bin/Debug/drawskt.m
@@ -1,14 +1,14 @@
 %USAGE: drawskt(1,3,1,4,1,2) --- show actions 1,2,3 performed by subjects 1,2,3,4 with instances 1 and 2.
 function drawskt()
 
-J=[20     1     2     1     8    10     2     9    11     3     4     7     7     5     6    14    15    16    17;
-    3     3     3     8    10    12     9    11    13     4     7     5     6    14    15    16    17    18    19];
+J=[4     3     3     3     2    1      1     5    6     7     9      10     11     13    14    15    17    18    19;
+   3     5     9     2     1    13     17    6    7     8     10     11     12     14    15    16    18    19    20];
 
 B=[];
 %for a=a1:a2
  %   for s=s1:s2
   %      for e=e1:e2
-            file=sprintf('17446472243_captured.txt');
+            file=sprintf('2534131250_captured_screen.txt');
             fp=fopen(file);
             if (fp>0)
                A=fscanf(fp,'%f');
@@ -32,9 +32,9 @@ P=B(:,:,4);
 for s=1:size(X,2)
     S=[X(:,s) Y(:,s) Z(:,s)];
   
-    xlim = [-800 800];
-    ylim = [-800 800];
-    zlim = [-800 800];
+    xlim = [0 800];
+    ylim = [0 800];
+    zlim = [0 800];
     set(gca, 'xlim', xlim, ...
              'ylim', ylim, ...
              'zlim', zlim);
@@ -42,7 +42,7 @@ for s=1:size(X,2)
     h=plot3(S(:,1),S(:,2),S(:,3),'r.');
     %rotate(h,[0 45], -180);
     set(gca,'DataAspectRatio',[1 1 1])
-    axis([0 400 0 400 0 400])
+    axis([-100 600 -100 600 -100 600])
 
     for j=1:19
         c1=J(1,j);
@@ -50,4 +50,8 @@ for s=1:size(X,2)
         line([S(c1,1) S(c2,1)], [S(c1,2) S(c2,2)], [S(c1,3) S(c2,3)]);
     end
     pause(1/20)
+    F(s) = getframe;
 end
+
+fig = figure;
+movie(fig, F, 2)
